23fa9b98f1fdd4b58da5e4c446aba60e3c40e098
Nathan Beaumont <nathanbeaumont@gruhub.com>
2020-10-29 21:43:51 -0400

    Initial commit

---
+++ White Mountain Four Thousand Footers/Source/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  White Mountain Four Thousand Footers
+//
+//  Created by Nathan Beaumont on 10/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ White Mountain Four Thousand Footers/Source/White_Mountain_Four_Thousand_FootersApp.swift
@@ -0,0 +1,17 @@
+//
+//  White_Mountain_Four_Thousand_FootersApp.swift
+//  White Mountain Four Thousand Footers
+//
+//  Created by Nathan Beaumont on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WhiteMountainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
